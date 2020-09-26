a42b1d6b49e4df1baf327bc03222593e876de5de
Mikhail Sonin <soninmikh@gmail.com>
2020-09-26 16:31:58 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace Practica_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList myList = new ArrayList(); myList.Add("First");
+            myList.Add("Second");
+            myList.Add("Third");
+            myList.Add("Fouth");
+
+            foreach(string item in myList)
+            {
+                Console.WriteLine("Unsorted: {0}",item);
+            }
+            myList.Sort();
+            foreach(string item in myList)
+            {
+                Console.WriteLine("Sorted: {0}", item);
+            }
+        }
+    }
+}
